a9f8a00145cd0f74e5ecf45610e5608519106297
kamal <kamal@gmail.com>
2024-02-21 19:09:34 +0200

    skills complite

--- src/component/skills/Skills.jsx
+++ src/component/skills/Skills.jsx
@@ -20,57 +20,67 @@ const mySkills = [
     startValue: 0,
   },
   {
-    id: 1,
+    id: 3,
     name: "Video Editing",
     percentage: 95,
     color: "#0dbab0",
     startValue: 0,
   },
-]
+];
 
 export default function Skills() {
-  const [skills, setSkills] = useState([...mySkills])
+  const [skills, setSkills] = useState([...mySkills]);
 
   useEffect(() => {
     const progress = setInterval(() => {
       setSkills((prevSkills) => {
         const updatedSkills = prevSkills.map((skill) => {
           const newStartValue = skill.startValue + 1;
-          if (newStartValue <= skill.percentage) {
-            return { ...skill, startValue: newStartValue }
-          } else {
-            return skill
-          }
+          return newStartValue <= skill.percentage
+            ? { ...skill, startValue: newStartValue }
+            : skill;
         });
+
+        // Check if all skills have reached their percentage
         const allSkillsComplete = updatedSkills.every(
           (skill) => skill.startValue > skill.percentage
-        )
+        );
+
         if (allSkillsComplete) {
-          clearInterval(progress)
+          clearInterval(progress);
         }
-        return updatedSkills
-      })
-    }, 50)
-    return () => clearInterval(progress)
-  }, [])
+
+        return updatedSkills;
+      });
+    }, 50);
+
+    return () => clearInterval(progress);
+  }, []);
+
   return (
     <div className="skills">
       <MainTitle title="Skills" />
       <div className="container">
         <div className="cards">
-
           {skills.map((x) => {
             return (
               <div key={x.id} className="card">
-                <div className="circular-progress" style={{ backgroundImage: `conic-gradient(${x.color} ${x.startValue * 3.6}deg, var(--main-color-white)  0deg)` }}>
-                  <span className="progress-value" style={{ color: x.color }}>%{x.startValue}</span>
+                <div
+                  className="circular-progress"
+                  style={{
+                    backgroundImage: `conic-gradient(${x.color} ${x.startValue * 3.6}deg, var(--main-color-white)  0deg)`,
+                  }}
+                >
+                  <span className="progress-value" style={{ color: x.color }}>
+                    %{x.startValue}
+                  </span>
                 </div>
-                <div className="test" style={{ color: x.color }}>{x.name}</div>
+                <div className="test" style={{ color: x.color }}>
+                  {x.name}
+                </div>
               </div>
-            )
+            );
           })}
-
-
         </div>
       </div>
     </div>
