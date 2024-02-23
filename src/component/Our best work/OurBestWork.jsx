c0a336447d472514642f4408f4f58249775a69b8
kamal <kamal@gmail.com>
2024-02-23 06:51:41 +0200

    after animation

--- src/component/Our best work/OurBestWork.jsx
+++ src/component/Our best work/OurBestWork.jsx
@@ -2,7 +2,7 @@
 import MainTitle from "../MainTitle";
 import "./OurBestWork.css";
 // import React
-import React, { useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 // ? import External libraries
 // import Swiper core and required modules
 import { Navigation, EffectFade} from 'swiper/modules';
@@ -13,13 +13,13 @@ import { Navigation, EffectFade} from 'swiper/modules';
   import 'swiper/css/navigation';
   import 'swiper/css/effect-fade';
 
-
   const myProjects = [
     {
       id: 1,
       ball: "../../../public/images/shapes/Our best work/ball-1.svg",
       imgIcon: "../../../public/images/shapes/Our best work/graphic-1.svg",
-      imgProject: "../../../public/images/shapes/Our best work/test-1.jpg"
+      imgProject: "../../../public/images/shapes/Our best work/test-1.jpg",
+
     },
 
     {
@@ -41,49 +41,66 @@ import { Navigation, EffectFade} from 'swiper/modules';
 
 
 
-export default function OurBestWork() {
+  export default function OurBestWork() {
+    const [sliderIndex, setSliderIndex] = useState(0)
 
-  // const [activeSlide, setActiveSlide] = useState(1);
+    useEffect(() => {
+      if(sliderIndex === 0) {
+        document.body.classList.add("purple")
+        document.body.classList.remove("yellow")
+        document.body.classList.remove("blue")
+      }else if(sliderIndex === 1) {
+        document.body.classList.add("yellow")
+        document.body.classList.remove("purple")
+        document.body.classList.remove("blue")
+      }else{
+        document.body.classList.add("blue")
+        document.body.classList.remove("yellow")
+        document.body.classList.remove("purple")
+      }
+    }, [sliderIndex])
 
 
-  return (
-    <div className="OurBestWork">
-      <MainTitle title="Our Best Work" />
-      <div className="container">
-        <Swiper
-          className="swiper"
-          modules={[Navigation, EffectFade]}
-          spaceBetween={10}
-          slidesPerView={1}
-          onSlideChange={(e) => console.log(e)}
-          onSwiper={(swiper) => console.log(swiper)}
-          loop={true}
-          navigation
-          effect={'fade'}
-        >
-
-        {myProjects.map((x) => {
-          return(
-            
-            <SwiperSlide key={x.id} className="swiper-slide">
-            <div className="content">
-              <div className="left">
-                <div className="ball"><img src={x.ball} alt="" /></div>
-                <div className="img-icon">
-                  <img src={x.imgIcon} alt="" />
+    return (
+      <div className="OurBestWork">
+        <MainTitle title="Our Best Work" />
+        <div className="container">
+          <Swiper
+            className="swiper"
+            modules={[Navigation, EffectFade]}
+            spaceBetween={10}
+            slidesPerView={1}
+            onSlideChange={(swiper) => setSliderIndex(swiper.activeIndex)}
+            onSwiper={(swiper) => console.log(swiper)}
+            loop={false}
+            navigation
+            // navigation={{
+            //   nextEl: "/public/images/goo.jpg",
+            //   prevEl: ".image-swiper-button-prev",
+            //   disabledClass: "swiper-button-disabled"
+            // }}
+            effect={'fade'}
+          >
+            {myProjects.map((x, index) => (
+              // <SwiperSlide key={x.id} className={`swiper-slide ${sliderIndex !== index && "none"}`}>
+              <SwiperSlide key={x.id} className={`swiper-slide`}>
+                <div className="content">
+                  <div className="left">
+                    <div className="ball">
+                      <img src={x.ball} alt="" />
+                    </div>
+                    <div className="img-icon">
+                      <img src={x.imgIcon} alt="" />
+                    </div>
+                  </div>
+                  <div className="right">
+                    <img src={x.imgProject} alt="" />
+                  </div>
                 </div>
-              </div>
-              <div className="right">
-                <img src={x.imgProject} alt="" />
-              </div>
-            </div>
-          </SwiperSlide>
-          
-          )
-        })}
-
-        </Swiper>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
